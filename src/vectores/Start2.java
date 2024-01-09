1ad4fc75d41ef6a79053fdc1b23e21fe1d39c880
pablo <pablo@DESKTOP-3AKGU8K>
2024-01-09 09:48:04 +0100

    cambio de prueba

--- src/vectores/Start2.java
+++ src/vectores/Start2.java
@@ -9,16 +9,7 @@ public class Start2 {
 		
 		char frase [] = new char[10];
 		
-		frase[0] = 'h';
-		frase[1] = 'o';
-		frase[2] = 'l';
-		frase[3] = 'a';
-		frase[4] = 'm';
-		frase[5] = 'u';
-		frase[6] = 'n';
-		frase[7] = 'd';
-		frase[8] = 'o';
-		frase[9] = '!';
+		
 		
 		
 		for(int i=frase.length-1;i>=0;i--) {
